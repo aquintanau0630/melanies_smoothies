d23a064cb9eef1340c55e6f453b283e6ba53492c
aquintanau <63994502+aquintanau0630@users.noreply.github.com>
2026-07-14 13:10:40 -0500

    Update streamlit_app.py
    add API Request

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,10 @@ if ingredients_list:
    st.write(my_insert_stmt)
 
    time_to_insert = st.button('Submit Order')
-   #st.stop()
+
+   import requests  
+   smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+   st.text(smoothiefroot_response)
 
    if time_to_insert:
       session.sql(my_insert_stmt).collect()
